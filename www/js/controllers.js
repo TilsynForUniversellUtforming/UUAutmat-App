5303773230ffba02c96fa95e2213f9d1d8856177
rogerww <trzy.akordy@gmail.com>
2016-04-08 13:51:45 +0200

    Working on overview page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,8 +19,25 @@ angular.module('appMain.controllers', [])
 })
 .controller('overviewCtrl', function ($scope, dummyData){
 
-    $scope.data = dummyData.getTyper();
-    console.log($scope.data);
+    $scope.testTyper = dummyData.getTyper();
+    console.log($scope.testTyper[0]);
+
+
+    var activeTestType={};
+    $scope.testObjects = [];
+    $scope.TestTypeClick = function(obj)
+    {
+        activeTestType = obj;
+        $scope.testObjects = dummyData.getTestObjekter(obj.ID);
+
+    }
+    $scope.changeActiveTestType = function(obj)
+    {
+        if(obj==activeTestType) return true;
+        return false;
+    }
+    
+
 
 
 
