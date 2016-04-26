b12ce4172dbdc11b8960d6fa6ffd2116b66ded5b
rogerww <trzy.akordy@gmail.com>
2016-04-26 18:06:34 +0200

    legger en del av logikken inn i en service

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -30,74 +30,26 @@ angular.module('appMain.controllers', [])
      }
 
 })
-.controller('newTestCtrl', function($scope, $stateParams, TestObjectService, IndicatorService){
-    $scope.template = $stateParams.temp;
-    $scope.indicators = [];
-    $scope.test_objects = [];
-    $scope.test_state={
-        current:{name:'',type:'',index:null},
-        previous:{name:'',type:''},
-        next:{name:'',type:''},
-        index:0,
-        states:[{name:'Generell Informasjon', type:'general_info'}, {name:'Virksomhet / Lokale', type:'virksomhet_lokale'}],
-        addIndicator:function(ind){
-            this.states.push(ind);
-        },
-        addTestObject:function(tobj){
-            this.states.push(tobj);
-        },
-        setStates:function(){
-            this.current=this.states[this.index];
-            if(this.index==0){this.previous=''}else{this.previous=this.states[this.index-1]}
-            if(this.index==(this.states.length-1)){this.next=''}else{this.next=this.states[this.index+1]}
-                console.log("Setting state: " + this.states[this.index].type)
-            console.log(this.states)
-        },
-        next2:function(){
-            console.log("lenght: " + this.states.length + " index: " + this.index)
-            console.log(this.states)
-            if(!(this.index >= this.states.length-1)){
-                this.index++;
-                this.setStates();
-                console.log("burp2")
-            }
-        },
-        previous2:function(){
+.controller('newTestCtrl', function($scope, $stateParams, TestObjectService, IndicatorService, TestService){
+    TestService.setTemplate($stateParams.temp);
+    $scope.test=TestService.getTemplate();
+    TestService.setupTest();
+    
+    $scope.indicators = TestService.getIndicators();
+    $scope.test_objects = TestService.getTestObjects();
+    $scope.test_state = TestService.state;
 
-            if(!this.index==0){
-                this.index--;
-                this.setStates();
-            }
-        }
-
-    };
-
-    $scope.test={
-        indicators:[],
-        test_objects:[]
+    $scope.test=function(){
+        console.log(TestService.getTemplate());
+        console.log(TestService.getIndicators());
     }
-    var t=0,t1=0;
-
-    for(i=0;i<$scope.template.test_objects.length;i++){
-        TestObjectService.get({id:$scope.template.test_objects[i]}, function(d){
-            $scope.test_objects.push(d);
-            var obj={name:d.name, type:'test_object', index:t}
-            t++;
-            $scope.test_state.addTestObject(obj);
-            $scope.test_state.setStates();
-        })
-    }
-    for(i=0; i < $scope.template.indicators.length; i++){
-        IndicatorService.get({id:$scope.template.indicators[i]}, function(r){
-            $scope.indicators.push(r);
-            var obj={name:r.name, type:'indicator', index:t1}
-            t1++;
-            $scope.test_state.addIndicator(obj);
-            $scope.test_state.setStates();
-        })
-    }
-
-
+   $scope.testf=function(o){
+    console.log("Hei")
+    console.log(o);
+   }
+   $scope.setRadioAltAnswer=function(input,alt){
+    input.answer=alt.text;
+   }
 })
 .controller('overviewCtrl', function ($scope, dummyData){
 
